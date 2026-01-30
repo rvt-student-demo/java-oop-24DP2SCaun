8b1ba5f9c2c64482f51d58f9e4237778ec81e27a
24DP2SCaun <a240352sc@rvt.lv>
2026-01-30 16:53:40 +0200

    pabeigts java mooc 6-2 to do list 2 parts kods

--- src/main/java/rvt/java_mooc_6_2_to_do_list.java
+++ src/main/java/rvt/java_mooc_6_2_to_do_list.java
@@ -1,6 +1,7 @@
 package rvt;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class java_mooc_6_2_to_do_list {
 
@@ -16,16 +17,57 @@ public class java_mooc_6_2_to_do_list {
     
     public void print(){
 
-        for(int i=0; i < this.tasks.size(); i++) {
-            System.out.println((i + 1) + ": " + this.tasks.get(i));
+        for(int i=0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ": " + tasks.get(i));
         }
     }
 
     public void remove(int number) {
 
-        int index = number - 1;
-        if (index >= 0 && index < this.tasks.size()) {
-            this.tasks.remove(index);
+        if (number >= 1 && number <= tasks.size()) {
+            tasks.remove(number - 1);
         }
+    }
+}
+
+
+class UserInterface {
+    private java_mooc_6_2_to_do_list list;
+    private Scanner scanner;
+
+    public UserInterface(java_mooc_6_2_to_do_list list, Scanner scanner) {
+        this.list = list;
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("stop")) {
+                break;
+            } else if (command.equals("add")) {
+                System.out.print("To add: ");
+                String task = scanner.nextLine();
+                list.add(task);
+            } else if (command.equals("list")) {
+                list.print();
+            } else if (command.equals("remove")) {
+                System.out.print("Which one is removed? ");
+                int number = Integer.parseInt(scanner.nextLine());
+                list.remove(number);
+            }
+        }
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        java_mooc_6_2_to_do_list list = new java_mooc_6_2_to_do_list();
+        Scanner scanner = new Scanner(System.in);
+        UserInterface ui = new UserInterface(list, scanner);
+        ui.start();
     }
 }
