c7822cae202c9cb13f9596b5f6820cdb49f9b43a
24DP2SCaun <a240352sc@rvt.lv>
2026-02-06 14:04:43 +0200

    izlabota kļūda kodā

--- src/main/java/registreties/tablePrinter.java
+++ src/main/java/registreties/tablePrinter.java
@@ -6,12 +6,12 @@ public class tablePrinter {  // klase, kas izdrukā tabulu konsolē
 
     public static void printstudents(List<student> students) { // metode tabulas izdrukai
 
-        String format = "| %-15s | %-15s | %-20s | %-19s | %n"; // formatēšanas šablons - nosaka kolonnu platumus
+        String format = "| %-15s | %-15s | %-35s | %-25s | %-19s | %n"; // formatēšanas šablons - nosaka kolonnu platumus
 
-        String line = "+---------------------+---------------------+---------------------+---------------------+"; // tabulas augšējā, apakšējā līnija
+        String line = "+---------------------+---------------------+---------------------+---------------------+--------------------+"; // tabulas augšējā, apakšējā līnija
 
         System.out.println(line); // izdrukā līniju
-        System.out.format(format, "Vārds", "Uzvārds", "Personas kods", "Reģistrācijas datums"); // virsraksti
+        System.out.format(format, "Vārds", "Uzvārds", "E-pasts", "Personas kods", "Reģistrācijas datums"); // virsraksti
         System.out.println(line);
 
         for (student s : students) { //izdrukā katru studentu
